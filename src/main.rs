bc8eb91c3d56ff9955c565f854bc7875fc8538eb
Roy Murphy <roycoins@gmail.com>
2025-08-05 07:07:58 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use tracing::info;
 
 use zip::config::env::EnvConfig;
 use zip::ui::components::App;
-use zip::utils::setup_logging;
+use zip::utils::logging::setup_logging;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = EnvConfig::load()?;
-    setup_logging(&config.log_level)?;
+    setup_logging(&config)?;
 
     info!("Initializing Zip wallet");
 
