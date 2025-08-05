5909034c01e27489106110cab3a0ad5f852c2799
Roy Murphy <roycoins@gmail.com>
2025-08-05 01:02:47 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use dioxus::prelude::*;
 use tracing::info;
-use zip::auth::OAuthManager;
-use zip::blockchain::WalletManager;
+use zip::auth::{OAuthManager, PasskeyManager};
+use zip::blockchain::{PaymailManager, TransactionManager, WalletManager};
 use zip::config::Config;
+use zip::integrations::RustBusIntegrator;
 use zip::storage::ZipStorage;
 use zip::ui::router::AppRouter;
 use zip::utils::setup_logging;
@@ -15,12 +16,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("Initializing Zip wallet");
 
     let storage = Arc::new(ZipStorage::new()?);
+    let rustbus = Arc::new(RustBusIntegrator::new("http://localhost:8080")?);
+    let tx_manager = Arc::new(TransactionManager::new(Arc::clone(&storage), Some(Arc::clone(&rustbus))));
+    let wallet = WalletManager::new(Arc::clone(&storage), Arc::clone(&tx_manager), Some(Arc::clone(&rustbus)))?;
     let oauth = OAuthManager::new(Arc::clone(&storage))?;
-    let tx_manager = Arc::new(TransactionManager::new(Arc::clone(&storage)));
-    let wallet = WalletManager::new(Arc::clone(&storage), tx_manager)?;
+    let passkey = PasskeyManager::new(Arc::clone(&storage))?;
+    let paymail = PaymailManager::new(PrivateKey::new(), Arc::clone(&storage));
 
-    // Launch Dioxus app
-    dioxus_desktop::launch(AppRouter, |c| c.with_context(wallet).with_context(oauth));
+    dioxus_desktop::launch(
+        AppRouter,
+        |c| {
+            c.with_context(wallet)
+                .with_context(oauth)
+                .with_context(passkey)
+                .with_context(paymail)
+                .with_context(tx_manager)
+                .with_context(rustbus)
+        },
+    );
 
     Ok(())
 }
