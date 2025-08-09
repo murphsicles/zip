7448c79212ef683233ff41addc41359db521a849
Roy Murphy <roycoins@gmail.com>
2025-08-09 08:09:40 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,13 @@
-use tracing::{Level, Subscriber};
-use tracing_subscriber::{EnvFilter, FmtSubscriber};
+use rand::rngs::thread_rng;
+use rand::RngCore;
+use tracing_subscriber::FmtSubscriber;
 
-use crate::config::Config;
+use crate::config::EnvConfig;
 use crate::errors::ZipError;
 
-pub fn setup_logging(config: &Config) -> Result<(), ZipError> {
+pub fn setup_logging(config: &EnvConfig) -> Result<(), ZipError> {
     let subscriber = FmtSubscriber::builder()
-        .with_env_filter(EnvFilter::new(&config.log_level))
+        .with_env_filter(config.log_level.clone())
         .finish();
     tracing::subscriber::set_global_default(subscriber)
         .map_err(|e| ZipError::Auth(e.to_string()))?;
@@ -15,6 +16,6 @@ pub fn setup_logging(config: &Config) -> Result<(), ZipError> {
 
 pub fn generate_salt(size: usize) -> Vec<u8> {
     let mut salt = vec![0u8; size];
-    rand::thread_rng().fill_bytes(&mut salt);
+    thread_rng().fill_bytes(&mut salt);
     salt
 }
