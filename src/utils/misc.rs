a7406c0478b287dbd572d4bf97ecb93c62ecbeb6
Roy Murphy <roycoins@gmail.com>
2025-08-09 21:58:04 +0100

    Create misc.rs

---
+++ src/utils/misc.rs
@@ -0,0 +1,18 @@
+use rand::rngs::OsRng;
+use rand::RngCore;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
+
+/// Generates a cryptographically secure random salt.
+pub fn generate_salt(len: usize) -> Vec<u8> {
+    let mut bytes = vec![0u8; len];
+    OsRng.fill_bytes(&mut bytes);
+    bytes
+}
+
+/// Sets up logging with tracing-subscriber based on EnvConfig log level.
+pub fn setup_logging(config: &crate::config::env::EnvConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let filter = EnvFilter::try_new(&config.log_level)?;
+    tracing_subscriber::registry().with(filter).init();
+    Ok(())
+}
