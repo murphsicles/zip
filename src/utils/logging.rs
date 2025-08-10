77edb677983ea77bff83daf7f7ef53cb8931ad2e
Roy Murphy <roycoins@gmail.com>
2025-08-10 18:28:45 +0100

    Update logging.rs

--- src/utils/logging.rs
+++ src/utils/logging.rs
@@ -1,13 +1,13 @@
+use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt;
 use tracing_subscriber::prelude::*;
-use tracing_subscriber::EnvFilter;
 
 use crate::config::EnvConfig;
 use crate::errors::ZipError;
 
 pub fn setup_logging(config: &EnvConfig) -> Result<(), ZipError> {
-    let filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new(&config.log_level));
+    let filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(&config.log_level));
     fmt()
         .with_env_filter(filter)
         .try_init()
