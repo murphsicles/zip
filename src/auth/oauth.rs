e3dcda202603d6fe7f911a2cfc3a079a441d76ec
Roy Murphy <roycoins@gmail.com>
2025-08-17 05:46:54 +0100

    Update oauth.rs

--- src/auth/oauth.rs
+++ src/auth/oauth.rs
@@ -2,8 +2,8 @@ use oauth2::basic::BasicClient;
 use oauth2::{
     AuthorizationCode, CsrfToken, PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, Scope,
 };
-use openidconnect::core::{CoreAdditionalClaims, CoreGenderClaim};
 use openidconnect::IdTokenClaims;
+use openidconnect::core::{CoreAdditionalClaims, CoreGenderClaim};
 use std::sync::Arc;
 
 use crate::errors::ZipError;
@@ -23,20 +23,16 @@ impl OAuth {
                 .parse()
                 .map_err(|_| ZipError::OAuth("Invalid client ID".to_string().into()))?,
         )
-        .set_client_secret(Some(
-            config
-                .oauth_client_secret
-                .parse()
-                .map_err(|_| ZipError::OAuth("Invalid client secret".to_string().into()))?,
-        ))
+        .set_client_secret(Some(config.oauth_client_secret.parse().map_err(|_| {
+            ZipError::OAuth("Invalid client secret".to_string().into())
+        })?))
         .set_auth_url(
             Url::parse(&config.oauth_auth_url)
                 .map_err(|_| ZipError::OAuth("Invalid auth URL".to_string().into()))?,
         )
-        .set_token_url(Some(
-            Url::parse(&config.oauth_token_url)
-                .map_err(|_| ZipError::OAuth("Invalid token URL".to_string().into()))?,
-        ))
+        .set_token_url(Some(Url::parse(&config.oauth_token_url).map_err(|_| {
+            ZipError::OAuth("Invalid token URL".to_string().into())
+        })?))
         .set_redirect_uri(RedirectUrl::from_url(
             Url::parse(&config.oauth_redirect_uri)
                 .map_err(|_| ZipError::OAuth("Invalid redirect URI".to_string().into()))?,
